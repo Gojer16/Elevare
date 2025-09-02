473e0e0f304f88fa1255eb4d7f1c1833831b7adb
Gojer <gojer@naver.com>
2025-09-02 08:42:52 -0400

    Constants file for team

---
+++ client/app/constants/team.ts
@@ -0,0 +1,10 @@
+import { FaUser, FaLightbulb } from "react-icons/fa";
+
+export const team = [
+  {
+    name: "Orlando",
+    role: "The Visionary",
+    icon: FaUser,
+    bio: "Founder and product engineer. Driving the mission to help people focus on what truly matters.",
+  },
+]
