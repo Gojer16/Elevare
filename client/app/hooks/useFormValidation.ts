eebf7f1f502ea20df6c524be107cc3267513dff6
Gojer <gojer@naver.com>
2025-09-05 06:57:00 -0400

    feat: custom hook for form validation

---
+++ client/app/hooks/useFormValidation.ts
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react';
+
+interface ValidationErrors {
+  [key: string]: string;
+}
+
+export default function useFormValidation<T>(
+  initialState: T,
+  validate: (values: T) => ValidationErrors,
+  submit: () => void
+) {
+  const [values, setValues] = useState<T>(initialState);
+  const [errors, setErrors] = useState<ValidationErrors>({});
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  useEffect(() => {
+    if (isSubmitting) {
+      const noErrors = Object.keys(errors).length === 0;
+      if (noErrors) {
+        try {
+          submit();
+        } finally {
+          setIsSubmitting(false);
+        }
+      } else {
+        setIsSubmitting(false);
+      }
+    }
+  }, [isSubmitting]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    const validationErrors = validate(values);
+    setErrors(validationErrors);
+    setIsSubmitting(true);
+  };
+
+  return { handleChange, handleSubmit, values, errors, isSubmitting };
+}
