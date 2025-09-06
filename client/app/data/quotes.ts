c17fe18f7b9f44fc8952fa86f7f5cc717165fb7a
Gojer <gojer@naver.com>
2025-09-06 14:39:32 -0400

    Quotes

---
+++ client/app/data/quotes.ts
@@ -0,0 +1,55 @@
+
+export const quotes = [
+  {
+    text: "Not everything matters equally.",
+    author: "Gary Keller, The ONE Thing",
+  },
+  {
+    text: "Extraordinary results are directly determined by how narrow you can make your focus.",
+    author: "Gary Keller, The ONE Thing",
+  },
+  {
+    text: "Success is sequential, not simultaneous.",
+    author: "Gary Keller, The ONE Thing",
+  },
+  {
+    text: "You do not rise to the level of your goals. You fall to the level of your systems.",
+    author: "James Clear, Atomic Habits",
+  },
+  {
+    text: "What’s the ONE Thing you can do today such that by doing it, everything else will be easier or unnecessary?",
+    author: "The ONE Thing",
+  },
+  {
+    text: "Focus is a matter of deciding what things you’re not going to do.",
+    author: "Gary Keller, The ONE Thing",
+  },
+  {
+    text: "The ability to focus is a skill you can develop.",
+    author: "Cal Newport, Deep Work",
+  },
+  {
+    text: "The key to success is to start before you are ready.",
+    author: "Marie Forleo, Everything is Figureoutable",
+  },
+    {
+    text: "Clarity about your ONE Thing is the starting point for success.",
+    author: "Gary Keller, The ONE Thing",
+  },
+ {
+    text: "You can do anything, but not everything.",
+    author: "Gary Keller, The ONE Thing",
+ }, 
+ {
+    text:" If you chase two rabbits, you will not catch either one.", 
+    author:"Russian Proverb"
+}, 
+{
+    text: "Every great change starts with a single step.",
+    author: "Lao Tzu"
+},
+{
+    text: "Every great change starts like falling dominoes.",
+    author: "BJ Thornton"
+}
+];
