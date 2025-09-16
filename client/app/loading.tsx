37e8860e1de0ec9b716d1c4b1c9ae437cb6c03ed
Gojer <gojer@naver.com>
2025-09-15 23:29:23 -0400

    Working now

--- client/app/loading.tsx
+++ client/app/loading.tsx
@@ -1,6 +1,4 @@
-'use client';
-import React from 'react';
-import { FaSpinner } from 'react-icons/fa';
+import { FaSpinner } from "react-icons/fa";
 
 export default function Loading() {
   return (
