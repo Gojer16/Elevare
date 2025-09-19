6f15fdded41dc3f215e61bc7b4a02cd2b5c83aab
Gojer <gojer@naver.com>
2025-09-19 07:35:11 -0400

    Fixing import path

--- client/app/dashboard/hooks/useTask.tsx
+++ client/app/dashboard/hooks/useTask.tsx
@@ -1,8 +1,7 @@
-// useTasks.tsx
 "use client";
 import { useCallback, useMemo, useState } from "react";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import type { Task } from "../../types/task";
+import type { Task } from "../../../types/task";
 
 export type { Task };
 
