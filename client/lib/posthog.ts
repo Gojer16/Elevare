4c663638e0b858f55466e1998ead97d4bfdd7db4
Gojer <gojer@naver.com>
2025-09-24 11:27:15 -0400

    Posthog

---
+++ client/lib/posthog.ts
@@ -0,0 +1,11 @@
+import posthog from "posthog-js";
+
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.posthog.com",
+    capture_pageview: true,  
+    capture_pageleave: true, 
+  });
+}
+
+export default posthog;
